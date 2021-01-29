96b7aed921fe01bb3355395a66c3f4f116153af2
Mateusz Łepecki <lepek__13@wp.pl>
2021-01-29 22:33:46 +0100

    A inserting timers to HTML

--- src/app/timer.js
+++ src/app/timer.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.switchTimers = exports.createTimers = exports.Timer = void 0;
+exports.insertTimerIntoDOM = exports.switchTimers = exports.createTimers = exports.Timer = void 0;
 var App_1 = require("./App");
+var MAIN = document.querySelector('#main-wrap');
+var DIV_ELEMENT = 'div';
+var cancelTimer = false;
+var DOMTimers = [];
 var Timer = /** @class */ (function () {
     function Timer(initialTime, color) {
         this.minutes = initialTime;
@@ -17,7 +21,7 @@ var Timer = /** @class */ (function () {
             if (_this.stopped)
                 clearInterval(interval);
             _this.seconds--;
-            _this.showRemainingTime();
+            // this.showRemainingTime();
             if (_this.seconds === 0 && _this.minutes === 0) {
                 _this.endOfTime();
                 clearInterval(interval);
@@ -33,9 +37,9 @@ var Timer = /** @class */ (function () {
             this.seconds = 60;
         }
     };
-    Timer.prototype.showRemainingTime = function () {
-        console.log(this.color + " - time left: " + this.minutes + ":" + this.seconds);
-    };
+    // showRemainingTime(): void {
+    //     console.log(`${this.color} - time left: ${this.minutes}:${this.seconds}`);
+    // }
     Timer.prototype.endOfTime = function () {
         console.log('End of time - you lose');
     };
@@ -43,15 +47,18 @@ var Timer = /** @class */ (function () {
 }());
 exports.Timer = Timer;
 var createTimers = function () {
-    var timerBlack = new Timer(App_1.gameSettings.choosenTime, 'black');
-    var timerWhite = new Timer(App_1.gameSettings.choosenTime, 'white');
-    timerWhite.startCounting();
+    cancelTimer = false;
+    var timerOne = new Timer(App_1.gameSettings.choosenTime, 'one');
+    var timerTwo = new Timer(App_1.gameSettings.choosenTime, 'two');
+    timerOne.startCounting();
     setTimeout(function () {
-        exports.switchTimers(timerWhite, timerBlack);
+        exports.switchTimers(timerOne, timerTwo);
     }, 10000);
     setTimeout(function () {
-        exports.switchTimers(timerWhite, timerBlack);
+        exports.switchTimers(timerOne, timerTwo);
     }, 24000);
+    DOMTimers = exports.insertTimerIntoDOM(timerOne, timerTwo);
+    updateDOMTimer(timerOne, timerTwo);
 };
 exports.createTimers = createTimers;
 var switchTimers = function (timerWhite, timerBlack) {
@@ -65,3 +72,48 @@ var switchTimers = function (timerWhite, timerBlack) {
     }
 };
 exports.switchTimers = switchTimers;
+var insertTimerIntoDOM = function (timerOne, timerTwo) {
+    var timersWrapper = createDOMElement(DIV_ELEMENT, 'timerWrapper', MAIN);
+    var player1timer = createDOMElement(DIV_ELEMENT, 'player1timer timer', timersWrapper);
+    var player2timer = createDOMElement(DIV_ELEMENT, 'player2timer timer', timersWrapper);
+    return [player1timer, player2timer];
+};
+exports.insertTimerIntoDOM = insertTimerIntoDOM;
+var createDOMElement = function (type, className, parent, text) {
+    if (text === void 0) { text = ''; }
+    var newElement = document.createElement(type);
+    newElement.className = className;
+    newElement.innerText = text;
+    parent.appendChild(newElement);
+    return newElement;
+};
+var changeTimeToString = function (minutes, seconds) {
+    var stringMinutes = minutes.toString();
+    var stringSeconds = seconds.toString();
+    if (minutes < 10) {
+        stringMinutes = '0' + stringMinutes;
+    }
+    if (seconds < 10) {
+        stringSeconds = '0' + stringSeconds;
+    }
+    if (seconds == 0)
+        stringSeconds = '00';
+    if (minutes == 0)
+        stringMinutes = '00';
+    return stringMinutes + ":" + stringSeconds;
+};
+var updateDOMTimer = function (timerOne, timerTwo) {
+    var player1timer = DOMTimers[0];
+    var player2timer = DOMTimers[1];
+    var interval = setInterval(function () {
+        if ((timerOne.minutes === 0 && timerOne.seconds == 0) ||
+            (timerTwo.minutes === 0 && timerTwo.seconds == 0) ||
+            cancelTimer)
+            clearInterval(interval);
+        player1timer.innerText = changeTimeToString(timerOne.minutes, timerOne.seconds);
+        player2timer.innerText = changeTimeToString(timerTwo.minutes, timerTwo.seconds);
+    }, 1000);
+};
+var turnTimerOffFaster = function () {
+    cancelTimer = true;
+};
--- src/app/timer.ts
+++ src/app/timer.ts
@@ -1,5 +1,10 @@
 import { gameSettings } from './App';
 
+const MAIN = document.querySelector('#main-wrap');
+const DIV_ELEMENT = 'div';
+let cancelTimer: boolean = false;
+let DOMTimers: HTMLElement[] = [];
+
 export class Timer {
     minutes: number;
     seconds: number;
@@ -13,47 +18,50 @@ export class Timer {
         this.color = color;
     }
 
-    startCounting() {
+    startCounting(): void {
         this.stopped = false;
         let interval = setInterval(() => {
             this.change60secondsTo1Minute();
             if (this.stopped) clearInterval(interval);
             this.seconds--;
-            this.showRemainingTime();
+            // this.showRemainingTime();
             if (this.seconds === 0 && this.minutes === 0) {
                 this.endOfTime();
                 clearInterval(interval);
             }
         }, 1000);
     }
-    stopCounting() {
+    stopCounting(): void {
         this.stopped = true;
     }
-    change60secondsTo1Minute() {
+    change60secondsTo1Minute(): void {
         if (this.seconds === 0 && this.minutes > 0) {
             this.minutes--;
             this.seconds = 60;
         }
     }
-    showRemainingTime(): void {
-        console.log(`${this.color} - time left: ${this.minutes}:${this.seconds}`);
-    }
-    endOfTime() {
+    // showRemainingTime(): void {
+    //     console.log(`${this.color} - time left: ${this.minutes}:${this.seconds}`);
+    // }
+    endOfTime(): void {
         console.log('End of time - you lose');
     }
 }
 
-export const createTimers = () => {
-    const timerBlack = new Timer(gameSettings.choosenTime, 'black');
-    const timerWhite = new Timer(gameSettings.choosenTime, 'white');
-    timerWhite.startCounting();
+export const createTimers = (): void => {
+    cancelTimer = false;
+    const timerOne = new Timer(gameSettings.choosenTime, 'one');
+    const timerTwo = new Timer(gameSettings.choosenTime, 'two');
+    timerOne.startCounting();
     setTimeout(() => {
-        switchTimers(timerWhite, timerBlack);
+        switchTimers(timerOne, timerTwo);
     }, 10000);
 
     setTimeout(() => {
-        switchTimers(timerWhite, timerBlack);
+        switchTimers(timerOne, timerTwo);
     }, 24000);
+    DOMTimers = insertTimerIntoDOM(timerOne, timerTwo);
+    updateDOMTimer(timerOne, timerTwo);
 };
 
 export const switchTimers = (timerWhite: Timer, timerBlack: Timer): void => {
@@ -64,4 +72,54 @@ export const switchTimers = (timerWhite: Timer, timerBlack: Timer): void => {
         timerBlack.stopCounting();
         timerWhite.startCounting();
     }
+};
+
+export const insertTimerIntoDOM = (timerOne: Timer, timerTwo: Timer): HTMLElement[] => {
+    const timersWrapper = createDOMElement(DIV_ELEMENT, 'timerWrapper', MAIN as HTMLElement);
+    const player1timer = createDOMElement(DIV_ELEMENT, 'player1timer timer', timersWrapper);
+    const player2timer = createDOMElement(DIV_ELEMENT, 'player2timer timer', timersWrapper);
+    return [player1timer, player2timer];
+};
+
+const createDOMElement = (type: string, className: string, parent: HTMLElement, text = ''): HTMLElement => {
+    const newElement = document.createElement(type);
+    newElement.className = className;
+    newElement.innerText = text;
+    parent.appendChild(newElement);
+    return newElement;
+};
+
+const changeTimeToString = (minutes: number, seconds: number): string => {
+    let stringMinutes = minutes.toString();
+    let stringSeconds = seconds.toString();
+
+    if (minutes < 10) {
+        stringMinutes = '0' + stringMinutes;
+    }
+    if (seconds < 10) {
+        stringSeconds = '0' + stringSeconds;
+    }
+    if (seconds == 0) stringSeconds = '00';
+    if (minutes == 0) stringMinutes = '00';
+    return `${stringMinutes}:${stringSeconds}`;
+};
+
+const updateDOMTimer = (timerOne: Timer, timerTwo: Timer) => {
+    const player1timer = DOMTimers[0];
+    const player2timer = DOMTimers[1];
+    let interval = setInterval(() => {
+        if (
+            (timerOne.minutes === 0 && timerOne.seconds == 0) ||
+            (timerTwo.minutes === 0 && timerTwo.seconds == 0) ||
+            cancelTimer
+        )
+            clearInterval(interval);
+
+        player1timer.innerText = changeTimeToString(timerOne.minutes, timerOne.seconds);
+        player2timer.innerText = changeTimeToString(timerTwo.minutes, timerTwo.seconds);
+    }, 1000);
+};
+
+const turnTimerOffFaster = () => {
+    cancelTimer = true;
 };
